02dfd044f0c85c873ebf134328d070aa82215db7
Ahmed Rezk <work72019@gmail.com>
2020-03-27 18:11:43 -0600

    React Router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Home from "./HomeComponent";
 import Directory from "./DirectoryComponent";
-import CampsiteInfo from "./CampsiteInfoComponent";
 import { CAMPSITES } from "../shared/campsites";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
@@ -19,21 +20,23 @@ class Main extends Component {
 	}
 
 	render() {
+		const HomePage = () => {
+			return <Home />;
+		};
 		return (
 			<>
 				<Header />
-				<Directory
-					campsites={this.state.campsites}
-					onClick={campsiteId => this.onCampsiteSelect(campsiteId)}
-				/>
-				<CampsiteInfo
-					campsite={
-						this.state.campsites.filter(
-							campsite =>
-								campsite.id === this.state.selectedCampsite
-						)[0]
-					}
-				/>
+				<Switch>
+					<Route path="/home" component={HomePage} />
+					<Route
+						exact
+						path="/directory"
+						render={() => (
+							<Directory campsites={this.state.campsites} />
+						)}
+					/>
+					<Redirect to="/home" />
+				</Switch>
 				<Footer />
 			</>
 		);
