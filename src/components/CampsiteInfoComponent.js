f47cf8b85633d222f783d00c00478fc6f76908e7
Ahmed Rezk <work72019@gmail.com>
2020-04-10 18:49:05 -0600

    Combining Reducers

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import {
 	Card,
 	CardImg,
@@ -6,9 +6,21 @@ import {
 	CardBody,
 	CardTitle,
 	Breadcrumb,
-	BreadcrumbItem
+	BreadcrumbItem,
+	Button,
+	Modal,
+	ModalBody,
+	ModalHeader,
+	Row,
+	Col,
+	Label,
 } from "reactstrap";
+import { Control, LocalForm, Errors } from "react-redux-form";
 import { Link } from "react-router-dom";
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
 
 function RenderCampsite({ campsite }) {
 	return (
@@ -39,16 +51,134 @@ function RenderComments({ comments }) {
 								{new Intl.DateTimeFormat("en-US", {
 									year: "numeric",
 									month: "short",
-									day: "2-digit"
+									day: "2-digit",
 								}).format(new Date(Date.parse(comment.date)))}
 							</p>
 						</div>
 					);
 				})}
+				<CommentForm />
 			</div>
 		);
 	}
 	return <div />;
+}
+
+// Comment Form
+class CommentForm extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			isModalOpen: false,
+		};
+
+		this.toggleModal = this.toggleModal.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	toggleModal() {
+		this.setState({
+			isModalOpen: !this.state.isModalOpen,
+		});
+	}
+
+	handleSubmit(values) {
+		console.log("Current state is: " + JSON.stringify(values));
+		alert("Current state is: " + JSON.stringify(values));
+	}
+
+	render() {
+		return (
+			<>
+				<Modal
+					isOpen={this.state.isModalOpen}
+					toggle={this.toggleModal}
+				>
+					<ModalHeader toggle={this.toggleModal}>
+						Submit Comment
+					</ModalHeader>
+					<ModalBody>
+						<LocalForm
+							onSubmit={(values) => this.handleSubmit(values)}
+						>
+							<Row className="form-group">
+								<Col>
+									<Label htmlFor="rating">Rating</Label>
+									<Control.select
+										model=".rating"
+										id=".rating"
+										name="rating"
+										className="form-control"
+									>
+										<option value="1">1</option>
+										<option value="2">2</option>
+										<option value="3">3</option>
+										<option value="4">4</option>
+										<option value="5">5</option>
+									</Control.select>
+								</Col>
+							</Row>
+
+							<Row className="form-group">
+								<Col>
+									<Label htmlFor="yourname">Your Name</Label>
+									<Control.text
+										model=".yourname"
+										id="yourname"
+										name="yourname"
+										placeholder="Your Name"
+										className="form-control"
+										validators={{
+											required,
+											minLength: minLength(2),
+											maxLength: maxLength(15),
+										}}
+									/>
+									<Errors
+										className="text-danger"
+										model=".yourname"
+										show="touched"
+										component="div"
+										messages={{
+											required: "Required",
+											minLength:
+												"Must be at least 2 characters",
+											maxLength:
+												"Must be 15 characters or less",
+										}}
+									/>
+								</Col>
+							</Row>
+
+							<Row className="form-group">
+								<Col>
+									<Label htmlFor="comment">Comment</Label>
+									<Control.textarea
+										model=".comment"
+										id="comment"
+										name="comment"
+										rows="12"
+										className="form-control"
+									/>
+								</Col>
+							</Row>
+							<Row className="form-group">
+								<Col>
+									<Button type="submit" color="primary">
+										Submit
+									</Button>
+								</Col>
+							</Row>
+						</LocalForm>
+					</ModalBody>
+				</Modal>
+				<Button outline color="secondary" onClick={this.toggleModal}>
+					<i className="fa fa-edit"></i> Submit Comment
+				</Button>
+			</>
+		);
+	}
 }
 
 function CompsiteInfo({ campsite, comments }) {
@@ -74,6 +204,11 @@ function CompsiteInfo({ campsite, comments }) {
 						<RenderCampsite campsite={campsite} />
 						<RenderComments comments={comments} />
 					</div>
+					{/* <div className="row">
+						<div className="col-md-5 m-1">
+							<CommentForm />
+						</div>
+					</div> */}
 				</div>
 			) : (
 				<div />
@@ -81,4 +216,5 @@ function CompsiteInfo({ campsite, comments }) {
 		</>
 	);
 }
+
 export default CompsiteInfo;
